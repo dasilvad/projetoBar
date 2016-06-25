ef0d23707f8d7dca56d5822ad87a40ae996f1220
dasilvad <silva10.daniel@gmail.com>
2016-06-25 01:31:41 -0300

    criado design tela Mesa, Consumo e Pedido

---
+++ Bar-MRI-Cliente-Mesa/src/PacotePrincipal/Produto.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PacotePrincipal;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author daniel
+ */
+public class Produto implements Serializable{
+    private String nome;
+    private String categoria;
+    private float preco;
+    private int quantidade;
+    private int id;
+            
+    public Produto(String nome, String categoria, float preco, int quantidade) {
+        this.nome = nome;
+        this.categoria = categoria;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+    
+    public int getId(){
+        return this.id;
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the categoria
+     */
+    public String getCategoria() {
+        return categoria;
+    }
+
+    /**
+     * @param categoria the categoria to set
+     */
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    /**
+     * @return the preco
+     */
+    public float getPreco() {
+        return preco;
+    }
+
+    /**
+     * @param preco the preco to set
+     */
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
+
+    /**
+     * @return the quantidade
+     */
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    /**
+     * @param quantidade the quantidade to set
+     */
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+}
